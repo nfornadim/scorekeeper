064e23e85ce17c4a25dbde1bacad618e820a69aa
Nadim <inspireze@gmail.com>
2018-10-19 23:54:09 +0530

    GUI improved

--- script.js
+++ script.js
@@ -1,61 +1,80 @@
-
-var p1Button = document.querySelector("#p1");
-var p2Button = document.querySelector("#p2");
-var resetButton = document.querySelector("#reset");
-var p1Display = document.querySelector("#p1Display");
-var p2Display = document.querySelector("#p2Display");
+var p1Button = document.querySelector("#p1Button");
+var p2Button = document.querySelector("#p2Button");
+var resetButton = document.querySelector("#resetButton");
 var numInput = document.querySelector("input");
+
+var p1Display = document.querySelector("#p1Score");
+var p1Body = document.querySelector(".card-A")
+
+var p2Display = document.querySelector("#p2Score");
+var p2Body = document.querySelector(".card-B")
+
+var winDisplay = document.querySelector("#winDisplay");
 
 var p1Score = 0;
 var p2Score = 0;
-
 var winningScore = 5;
 var gameOver = false;
 
-	p1Button.addEventListener("click", function() {
-		if (!gameOver) {
-			p1Score++;
-			if (winningScore === p1Score) {
-				p1Display.classList.add("winner");
-				gameOver = true;
-			}
-		} 
- 		
- 		p1Display.textContent = p1Score;
+p1Button.addEventListener("click", function() {
 	
-	});
+
+	if(!gameOver) {
+
+		p1Score++;
+			
+			if(p1Score === winningScore) {
+			p1Body.classList.add("winner");
+			gameOver = true;
+		}
+	}
+	
+	p1Display.textContent = p1Score;
+
+
+});
 
 	p2Button.addEventListener("click", function() {
-		if (!gameOver) {
-			p2Score++;
+
+	if (!gameOver) {
+		
+		p2Score++;
+
 			if (p2Score === winningScore) {
-				p2Display.classList.add("winner");
-				gameOver = true;
-			}
+			gameOver = true;
+			p2Body.classList.add("winner");
 		}
+ 	}
 	
-		p2Display.textContent = p2Score;
-	});
+	p2Display.textContent = p2Score;
 
-	resetButton.addEventListener("click", function() {
-		reset();
-	});
+});
 
-	function reset() {
-		p1Score = 0;
-		p2Score = 0;
-		p1Display.textContent = 0;
-		p2Display.textContent = 0;
-		p1Display.classList.remove("winner");
-		p2Display.classList.remove("winner");
-		gameOver = false;
-	}
+resetButton.addEventListener("click", function() {
 
-	numInput.addEventListener("change", function() {
+	reset();
 
-		winnerScore.textContent = this.value;
-		winningScore = Number(this.value);
-		reset();
 
-	});
+});
+
+numInput.addEventListener("change", function() {
+
+	winDisplay.textContent = this.value;
+
+	winningScore = Number(this.value);
+	//As typeof this.value is string so I added "Number" to fix this bug.
+
+	reset();
+});
+
+function reset() {
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Body.classList.remove("winner");
+	p2Body.classList.remove("winner");
+	gameOver = false;
+}
+
 
